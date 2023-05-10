3623dddb7f9ac0ab9487ae11dccd2b1355cd25f5
unknown <jprasanth7989@gmail.com>
2023-05-10 13:24:52 +0530

    third commit

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a51qp(daun=0r13of5t*m68zieer+%n#+xfg8$$b8w-sc@g@*s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
